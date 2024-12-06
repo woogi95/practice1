dcfb53e864487b5078ab0c6e8dbafabd69401afa
woogi95 <sdw951216@gmail.com>
2024-12-06 13:00:23 +0900

    [폴더 생성]

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,15 @@
+import Footer from "../componets/footer/Footer";
+import Header from "../componets/header/Header";
+import Main from "../componets/main1/Main";
+
+function IndexPage() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default IndexPage;
